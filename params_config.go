5acc1f989fd2a0bf43b2b583233a9cb6c4a8b0dd
GBTNetwork <114756890+alexwambu@users.noreply.github.com>
2025-09-17 14:38:08 +0300

    Delete params_config.go

--- params_config.go
+++
@@ -1,9 +0,0 @@
-package params
-
-var (
-    GBTNetworkChainID = 999
-    NetworkName       = "GBTNetwork"
-    CurrencySymbol    = "GBT"
-    RPCPort           = 9636
-    WSPort            = 9637
-)
